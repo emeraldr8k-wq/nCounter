8b551da666ac9bb388e8c8d34b7ac3e198dfa42b
emeraldr8k-wq <emeraldr8k@gmail.com>
2026-01-13 22:12:30 +0300

    z

--- src/App.java
+++ src/App.java
@@ -1,4 +1,57 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class App {
+    private static final Object lock = new Object();
+    private static int currentThread = 0;
+    private static int n;
+    private static int totalThread;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите кол во потоков");
+        totalThread = sc.nextInt();
+//        System.out.println("Введите кол во повторений");
+//        n = sc.nextInt();
+
+//        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < totalThread; j++) {
+                final int myNumber = j;
+
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (true) {
+                            synchronized (lock) {
+                                if (currentThread >= totalThread){
+                                    break;
+                                }
+                                if (currentThread != myNumber) {
+                                    try {
+                                        lock.wait();
+                                    } catch (InterruptedException e) {
+                                        throw new RuntimeException(e);
+                                    }
+
+                                } else {
+                                    System.out.println("Поток " + myNumber);
+//                                    n -= currentThread >= n ?(currentThread / n): 0;
+                                    currentThread = (currentThread + 1) ;
+
+                                }
+
+
+                                lock.notifyAll();
+
+                            }
+                        }
+                    }
+                });
+
+                thread.start();
+            }
+
+//        }
+
+
+    }
 }
